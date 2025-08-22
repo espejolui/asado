b52d59e596b4e44523f901af5e3a82c99c535846
Luis Espejo <herrera.espejo.luis@gmail.com>
2025-08-22 21:56:02 +0000

    create list ingredients

---
+++ helpers/list.js
@@ -0,0 +1,80 @@
+export const list = [
+    {
+        ingredient: "Carne",
+        value: "$18.000",
+        amount: "6 libras",
+        emoji: "🍖"
+    },
+     {
+        ingredient: "Carne",
+        value: "$18.000",
+        amount: "6 libras",
+         emoji: "🍖"
+    },
+     {
+        ingredient: "Carne",
+        value: "$18.000",
+        amount: "6 libras",
+         emoji: "🍖"
+    },
+     {
+        ingredient: "Carne",
+        value: "$18.000",
+        amount: "6 libras",
+         emoji: "🍖"
+    },
+      {
+        ingredient: "Carne",
+        value: "$18.000",
+        amount: "6 libras",
+         emoji: "🍖"
+    },
+     {
+        ingredient: "Carne",
+        value: "$18.000",
+        amount: "6 libras",
+         emoji: "🍖"
+    },
+     {
+        ingredient: "Carne",
+        value: "$18.000",
+        amount: "6 libras",
+         emoji: "🍖"
+    },
+     {
+        ingredient: "Carne",
+        value: "$18.000",
+        amount: "6 libras",
+         emoji: "🍖"
+    },
+      {
+        ingredient: "Carne",
+        value: "$18.000",
+        amount: "6 libras",
+         emoji: "🍖"
+    },
+     {
+        ingredient: "Carne",
+        value: "$18.000",
+        amount: "6 libras",
+         emoji: "🍖"
+    },
+     {
+        ingredient: "Carne",
+        value: "$18.000",
+        amount: "6 libras",
+         emoji: "🍖"
+    },
+     {
+        ingredient: "Carne",
+        value: "$18.000",
+        amount: "6 libras",
+         emoji: "🍖"
+    },
+    {
+        ingredient: "Carne",
+        value: "$18.000",
+        amount: "6 libras",
+         emoji: "🍖"
+    }
+]
